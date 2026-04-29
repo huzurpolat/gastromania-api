d7a0f4ae84dc39c210ac6e44651f201c20dd0aca
Huzur Polat <huzur.polat@gmx.net>
2026-04-29 08:39:22 +0200

    login

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -6,31 +6,41 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
 import { LocationsService } from './locations.service';
 
 @Controller('locations')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createLocationDto: CreateLocationDto) {
     return this.locationsService.create(createLocationDto);
   }
 
   @Get()
+  @Roles(Role.Admin, Role.Filialleiter, Role.Service, Role.Kueche, Role.Lager)
   findAll() {
     return this.locationsService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.Admin, Role.Filialleiter, Role.Service, Role.Kueche, Role.Lager)
   findOne(@Param('id') id: string) {
     return this.locationsService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(Role.Admin, Role.Filialleiter)
   update(
     @Param('id') id: string,
     @Body() updateLocationDto: UpdateLocationDto,
@@ -39,6 +49,7 @@ export class LocationsController {
   }
 
   @Delete(':id')
+  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.locationsService.remove(id);
   }
--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthJwtModule } from '../auth/auth-jwt.module';
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
 import { Location, LocationSchema } from './schemas/location.schema';
 
 @Module({
   imports: [
+    AuthJwtModule,
     MongooseModule.forFeature([
       { name: Location.name, schema: LocationSchema },
     ]),
